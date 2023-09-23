8fc58f23b209f1f3c01b7ff94622c22e921b6fe3
apilina <shroomshi@gmail.com>
2023-09-23 18:44:04 -0400

    Complete functions

--- Polynomial.java
+++ Polynomial.java
@@ -14,24 +14,44 @@ public class Polynomial {
 
 	public Polynomial add(Polynomial p) {
 		int max = 0;
+		int min = 0;
 
 		if((p.coefficients).length<(this.coefficients).length) {
 			max = (this.coefficients).length;
+			min = (p.coefficients).length;
 		}
 		else if((p.coefficients).length>(this.coefficients).length) {
 			max = (p.coefficients).length;
+			min = this.coefficients.length;
 		}
-
-		return;
+		
+		double[] arr = new double[max];
+		
+		for(int i=0; i<max; i++) {
+			if(i>=min) {
+				if(p.coefficients.length < max) {
+					arr[i] = this.coefficients[i];
+				}
+				else if(this.coefficients.length < max) {
+					arr[i] = p.coefficients[i];
+				}
+			}
+			else {
+				arr[i] = this.coefficients[i] + p.coefficients[i];
+			}
+		}
+		Polynomial temp = new Polynomial(arr);
+		
+		return temp;
 	}
 
 	public double evaluate(double x) {
 		int len = coefficients.length;
-		int total = 0;
+		double total = 0;
 		int xc = 0;
 
 		for(int i=0; i<len; i++) {
-			total = coefficients[i]*(Math.pow(x, xc));
+			total += coefficients[i]*(Math.pow(x, xc));
 			xc++;
 		}
 
@@ -39,5 +59,12 @@ public class Polynomial {
 	}
 
 	public boolean hasRoot(double x) {
-		//check if x is a root of the polymonial
+		//check if x is a root of the polynomial
+		if(evaluate(x)==0) {
+			return true;
+		}
+		
+		return false;
 	}
+}
+
