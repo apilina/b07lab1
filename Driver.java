de61f251896567806c2e34977c1bdeb67a3d82e7
apilina <shroomshi@gmail.com>
2023-10-01 20:19:54 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,15 +2,58 @@ public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		
+		double [] c1 = {6,5};
+		double [] e1 = {0,3};
+		Polynomial p1 = new Polynomial(c1, e1);
+		p1.printpoly();
+		
+		double [] c2 = {-2,-9};
+		double [] e2 = {1, 4};
+		Polynomial p2 = new Polynomial(c2, e2);
+		p2.printpoly();
+		
+		double [] c3 = {3,-2, 1};
+		double [] e3 = {1, 3, 4};
+		Polynomial p3 = new Polynomial(c3, e3);
+		p3.printpoly();
+		
+		double [] c4 = {};
+		double [] e4 = {};
+		Polynomial p4 = new Polynomial(c4, e4);
+		p4.printpoly();
+		
+//		Polynomial s = p2.add(p3);
+//		s.printpoly();
 		Polynomial s = p1.add(p2);
+		s.printpoly();
+		
+		Polynomial t = p1.add(p4);
+		t.printpoly();
+		
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		if(s.hasRoot(0.1))
+			System.out.println("0.1 is a root of s");
+		else
+			System.out.println("0.1 is not a root of s");
+		
+		System.out.println("s(1) = " + s.evaluate(1));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
+		
+//		Polynomial p = new Polynomial();
+//		System.out.println(p.evaluate(3));
+//		double [] c1 = {6,0,0,5};
+//		Polynomial p1 = new Polynomial(c1);
+//		double [] c2 = {0,-2,0,0,-9};
+//		Polynomial p2 = new Polynomial(c2);
+//		Polynomial s = p1.add(p2);
+//		System.out.println("s(0.1) = " + s.evaluate(0.1));
+//		if(s.hasRoot(1))
+//			System.out.println("1 is a root of s");
+//		else
+//			System.out.println("1 is not a root of s");
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,51 +1,146 @@
+import java.io.File;
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Polynomial {
 	double[] coefficients;
+	double[] exponents;
 
 	public Polynomial() {
 		coefficients = new double[] {0};
+		exponents = new double[] {0};
 	}
 
-	public Polynomial(double[] arr) {
-		coefficients = new double[arr.length];
-		for(int i=0; i<arr.length; i++) {
-			coefficients[i] = arr[i];
+	public Polynomial(double[] coef, double[] exp) {
+		coefficients = new double[coef.length];
+		exponents = new double[exp.length];
+		
+		for(int i=0; i<coef.length; i++) {
+			coefficients[i] = coef[i];
+		}
+		for(int j=0; j<exp.length; j++) {
+			exponents[j] = exp[j];
 		}
 	}
 
+//	public Polynomial(File f) {
+//		Scanner sc = new Scanner(f);
+//	}
+	
+	public void printpoly() {
+		if(this.coefficients.length == 0) {
+			System.out.println("nothing here");
+		}
+		else {
+			for(int i=0; i<this.coefficients.length; i++) {
+				System.out.print(this.coefficients[i]+"x^"+this.exponents[i]);
+				if(i!=this.coefficients.length-1) {
+					System.out.print(" + ");
+				}
+			}
+		}
+		System.out.print("\n");
+	}
+	
 	public Polynomial add(Polynomial p) {
 		int max = 0;
 		int min = 0;
-
-		if((p.coefficients).length<(this.coefficients).length) {
-			max = (this.coefficients).length;
-			min = (p.coefficients).length;
+		
+		int plen = p.exponents.length;
+		int tlen = this.exponents.length;
+		
+		if(plen == 0) {
+			return this;
 		}
-		else if((p.coefficients).length>(this.coefficients).length) {
-			max = (p.coefficients).length;
-			min = this.coefficients.length;
+		else if(tlen == 0) {
+			return p;
+		}
+		//find max size of new poly
+		if(p.exponents[plen-1] <= this.exponents[tlen-1]) {
+			max = (int)(this.exponents[tlen-1])+1;
+			min = plen;
 		}
 		else {
-			max = p.coefficients.length;
-			min = max;
+			max = (int)(p.exponents[plen-1])+1;
+			min = tlen;
 		}
 		
-		double[] arr = new double[max];
+		double[] coeftemp = new double[max];
+		double[] exptemp = new double[max];
+		int pf = -1;
+		int tf = -1;
 		
+		//add arrays
 		for(int i=0; i<max; i++) {
-			if(i>=min) {
-				if(p.coefficients.length < max) {
-					arr[i] = this.coefficients[i];
+			exptemp[i] = i;
+			
+			for(int j=0; j<max; j++) {
+				if(j==plen) {
+					break;
 				}
-				else if(this.coefficients.length < max) {
-					arr[i] = p.coefficients[i];
+				else {
+					if(p.exponents[j] == i) {
+						pf = j;
+						break;
+					}
 				}
+			}
+			
+			for(int j=0; j<max; j++) {
+				if(j==tlen) {
+					break;
+				}
+				else {
+					if(this.exponents[j] == i) {
+						tf = j;
+						break;
+					}
+				}
+			}
+			
+			if(pf!=(-1) && tf!=(-1)) {
+				coeftemp[i] = p.coefficients[pf]+this.coefficients[tf];
+			}
+			else if(pf!=(-1)) {
+				coeftemp[i] = p.coefficients[pf];
+			}
+			else if(tf!=(-1)) {
+				coeftemp[i] = this.coefficients[tf];
 			}
 			else {
-				arr[i] = this.coefficients[i] + p.coefficients[i];
+				coeftemp[i] = 0;
 			}
-			//System.out.println(arr[i]);
+			
+			pf = -1;
+			tf = -1;
+			for(int k=0; k<max; k++) {
+				System.out.println(coeftemp[k]+", "+exptemp[k]);
+			}
+			System.out.print("\n");
 		}
-		Polynomial temp = new Polynomial(arr);
+		
+		int finallen = 0;
+		//find len of array without 0s
+		for(int i = 0; i<max; i++) {
+			if(coeftemp[i] != 0) {
+				finallen++;
+			}
+		}
+		
+		double[] coef = new double[finallen];
+		double[] exp = new double[finallen];
+		int index = 0;
+		//fill in final arrays without 0s
+		for(int i = 0; i<max; i++) {
+			if(coeftemp[i] != 0) {
+				coef[index] = coeftemp[i];
+				exp[index] = exptemp[i];
+				index++;
+				System.out.println("added "+coeftemp[i]+" and "+exptemp[i]);
+			}
+		}
+		
+		Polynomial temp = new Polynomial(coef, exp);
 		
 		return temp;
 	}
@@ -53,11 +148,9 @@ public class Polynomial {
 	public double evaluate(double x) {
 		int len = coefficients.length;
 		double total = 0;
-		int xc = 0;
 
 		for(int i=0; i<len; i++) {
-			total += coefficients[i]*(Math.pow(x, xc));
-			xc++;
+			total += coefficients[i]*(Math.pow(x, exponents[i]));
 			//System.out.println(total);
 		}
 
@@ -71,6 +164,20 @@ public class Polynomial {
 		}
 		
 		return false;
+	}
+	
+	public Polynomial multiply(Polynomial p) {
+		int plen = p.exponents.length;
+		int tlen = this.exponents.length;
+		
+		if(plen == 0 || tlen==0) {
+			Polynomial temp = new Polynomial();
+			return temp;
+		}
+		
+		
+		for(int i=0; i<plen)
+		return p;
 	}
 }
 
